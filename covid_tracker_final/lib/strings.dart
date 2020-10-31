106c42bcbb3a24f21b856813e20c5fcaa3b33da5
muiz6 <muizhassan83@gmail.com>
2020-10-31 23:18:13 +0500

    created bottom appbar

---
+++ covid_tracker_final/lib/strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  static const String TITLE = "Covid App";
+  static const String GLOBAL = "Global";
+  static const String PAKISTAN = "Pakistan";
+  static const String INFO = "Info";
+  static const String COUNTRIES = "Countries";
+}
